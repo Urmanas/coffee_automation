3d3632b575d56f99b549dbf500b0ee2cc21d2c3a
Urmanas <Urmanas.19@gmail.com>
2025-12-22 14:48:59 +0600

    Add Orders model and initial migrations

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Orders(models.Model):
+    customer_name = models.CharField(max_length=100)
+    items = models.TextField()
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('new', 'New'),
+            ('ready', 'Ready'),
+            ('done', 'Done')
+        ],
+        default='new'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
