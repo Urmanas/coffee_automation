f68406281bfe20112218dc81ed15d4fe546ca005
Urmanas <Urmanas.19@gmail.com>
2025-12-24 18:07:21 +0600

    Add product and category models

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name 
+
+        
 class Order(models.Model):
     customer_name = models.CharField(max_length=100)
     items = models.TextField()
